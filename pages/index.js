79844ec0b77aa61f68da02aa878cb7570d0dfaf5
unknown <mada.lawliet@gmail.com>
2021-06-21 20:34:34 +0700

    sixth commit

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,16 @@ const HomePage = ({ products }) => {
 
 export async function getStaticProps() {
   // const res = await fetch(`http://localhost:3000/api/products`); for local
-  const res = await fetch(`https://sayur-app.vercel.app/api/products`);
+  const res = await fetch(`https://sayur-app.vercel.app/api/products`,
+  {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+      Accept: "application/json; charset=UTF-8",
+    },
+  });
   const products = await res.json();
 
   return {
--- pages/product/[id].js
+++ pages/product/[id].js
@@ -39,10 +39,28 @@ const ProductDetail = ({ product }) => {
 };
 
 export async function getStaticPaths() {
-  const res = await fetch(`https://sayur-app.vercel.app/api/products`);
+  const res = await fetch(`https://sayur-app.vercel.app/api/products`,
+  {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+      Accept: "application/json; charset=UTF-8",
+    },
+  });
   const products = await res.json();
 
-  const paths = products.list.map((prod) => `/product/${prod.id}`);
+  const paths = products.list.map((prod) => `/product/${prod.id}`,
+  {
+    method: "GET",
+    headers: {
+      // update with your user-agent
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36", 
+      Accept: "application/json; charset=UTF-8",
+    },
+  });
 
   return { paths, fallback: false };
 }
